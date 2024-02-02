96dbdabf2630e71ff9c06981a198ef7e1fa78808
johrielrsrd <johrielrsrd@gmail.com>
2024-02-02 23:32:39 +0800

    intitial commit.

---
+++ TodoWebApiProject/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoWebApiProject.Models;
+
+namespace TodoWebApiProject.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ToDo>toDos { get; set; }
+    }
+}
