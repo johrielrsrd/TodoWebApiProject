ebf4509e90b3bdde1e0512c0fcec74c36aa75cff
johrielrsrd <johrielrsrd@gmail.com>
2024-02-02 23:00:43 +0800

    Add project files.

---
+++ TodoWebApiProject/Models/ToDo.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoWebApiProject.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
